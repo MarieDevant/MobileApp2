432ae2767e1ceaa5ec798153f4e5c36b80e465b9
MarieDevant <marie.devant@hotmail.fr>
2017-10-05 14:28:39 +1000

    All pages began
    The radio button not done...
    Menu and navigation not done...

---
+++ MobileApp2/MobileApp2/MobileApp2/View/RemoveObject.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MobileApp2.View
+{
+    public partial class RemoveObject : ContentPage
+    {
+        public RemoveObject()
+        {
+            InitializeComponent();
+			StackLayout grid = AddHeader();
+			this.Content = grid;
+			grid.Children.Add(AddContent());
+
+		}
+		public StackLayout AddHeader()
+		{
+
+			Label title = new Label
+			{
+				Text = "Remove Object",
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.Center,
+				FontSize = 30,
+				VerticalTextAlignment = TextAlignment.Start,
+				HeightRequest = 70,
+				TextColor = Color.Black
+			};
+			StackLayout head = new StackLayout
+			{
+				TranslationY = 70,
+				Padding = new Thickness(30, 0),
+				Children =
+				{
+					title
+				}
+			};
+			return head;
+		}
+        public StackLayout AddContent(){
+			Entry name = new Entry
+			{
+				Text = "Item name",
+				HorizontalOptions = LayoutOptions.Start,
+				VerticalOptions = LayoutOptions.Center,
+				FontSize = 20,
+                WidthRequest = 200,
+				TextColor = Color.Black
+			};
+			Button remove = new Button
+			{
+				Text = "Remove",
+				TranslationY = 50,
+				VerticalOptions = LayoutOptions.End,
+				BackgroundColor = Color.RoyalBlue,
+				TextColor = Color.White
+			};
+			StackLayout content = new StackLayout
+			{
+				Padding = new Thickness(30, 0),
+				Children =
+				{
+					name,
+                    remove
+				}
+			};
+
+            return content;
+        }
+    }
+}
