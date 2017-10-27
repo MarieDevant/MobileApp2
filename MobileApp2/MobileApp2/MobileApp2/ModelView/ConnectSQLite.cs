821a31531acd96b256d0cc729a8976281ee84994
n9932305 <n9932305@VDI-VL04-002>
2017-10-27 10:47:24 +1000

    test SQLite, don't take into account

---
+++ MobileApp2/MobileApp2/MobileApp2/ModelView/ConnectSQLite.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.Sqlite;
+
+namespace MobileApp2.ModelView
+{
+    class ConnectSQLite
+    {
+        public SQLiteConnection my_co;
+
+        public ConnectSQLite() {
+            my_co = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+        }
+
+        public void OpenCo()
+        {
+            my_co.Open();
+        }
+
+        public void CloseCo()
+        {
+            my_co.Close();
+        }
+    }
+}
