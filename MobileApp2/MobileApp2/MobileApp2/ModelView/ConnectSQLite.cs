4d1b25b19d1f2394a335b8642d87774ed209bf42
masterjecht <dan_redguard@hotmail.com>
2017-10-28 16:11:04 +1000

    none

--- MobileApp2/MobileApp2/MobileApp2/ModelView/ConnectSQLite.cs
+++ MobileApp2/MobileApp2/MobileApp2/ModelView/ConnectSQLite.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Data.Sqlite;
+using System.Data;
+using Mono.Data.Sqlite;
+
 
 namespace MobileApp2.ModelView
 {
     class ConnectSQLite
     {
-        public SQLiteConnection my_co;
+        public SqliteConnection my_co;
 
         public ConnectSQLite() {
-            my_co = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+            my_co = new SqliteConnection("Data Source=MyDatabase.sqlite;Version=3;");
         }
 
         public void OpenCo()
