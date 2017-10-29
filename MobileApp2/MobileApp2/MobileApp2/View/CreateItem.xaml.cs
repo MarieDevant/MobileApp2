9d00130192a5d52ad0d57c776a821d40e318b73d
masterjecht <dan_redguard@hotmail.com>
2017-10-29 22:37:49 +1000

    Revert "small changes"
    This reverts commit 4950bc5ee56a0dd2f01f9a2482d71bdb6d805422.

--- MobileApp2/MobileApp2/MobileApp2/View/CreateItem.xaml.cs
+++ MobileApp2/MobileApp2/MobileApp2/View/CreateItem.xaml.cs
@@ -1,12 +1,8 @@
-﻿using System;
+﻿using MobileApp2.ModelView;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-
 
 namespace MobileApp2.View
 {
