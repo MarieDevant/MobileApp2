443541b8de2c66ceae386384694b26aa70f796ed
masterjecht <dan_redguard@hotmail.com>
2017-10-30 11:06:55 +1000

    Revert "Revert "none""
    This reverts commit b506297a881632daa9e35b5418b0362033b95d1a.

--- MobileApp2/MobileApp2/MobileApp2/View/TitlePage.xaml.cs
+++ MobileApp2/MobileApp2/MobileApp2/View/TitlePage.xaml.cs
@@ -58,39 +58,8 @@ namespace MobileApp2.View
         public TitlePage()
         {
             MyDatabase db = new MyDatabase();
-//            db.Insert(new ToDoItem()
-  //          {
-
-    //        });
           List<ToDoItem> items = db.GetAllItems();
-            // Initialize a fake database
-            /*
-            User me = new User("me", "pwd");
-            MoveOut vacance = new MoveOut("vacances", me);
-            Room r1 = new Room("Living Room", ConsoleColor.Red, vacance);
-            Box b11 = new Box("B1LR", r1);
-            Item it111 = new Item("Lamp", b11);
-            Item it112 = new Item("Pillow", b11);
-            Box b12 = new Box("B2LR", r1);
-            Item it121 = new Item("Blap", b12);
-            Room r2 = new Room("Kitchen", ConsoleColor.Green, vacance);
-            Box b21 = new Box("B1K", r2);
-            Item it211 = new Item("Plates", b21);
-            Item it212 = new Item("Forks x24", b21);
-            Box b22 = new Box("B2K", r2);
-            Item it221 = new Item("Glasses", b22);
-            Room r3 = new Room("Garage", ConsoleColor.Blue, vacance);
-            Box b31 = new Box("B1G", r3);
-            Item it311 = new Item("Bike", b31);
 
-
-            List<Room> rooms = new List<Room>();
-            rooms.Add( r1);
-            rooms.Add(r2);
-            rooms.Add(r3);
-
-            */
-            // Load lo = new Load(me, vacance, rooms);
 
             InitializeComponent();
 
@@ -115,20 +84,12 @@ namespace MobileApp2.View
             Textlayout.Children.Add(header);
             Textlayout.Children.Add(subTitle);
 
-<<<<<<< HEAD
 
              StackLayout grid = AddHeader();
              this.Content = grid;
             for (int i = 0; i < App.lo.MoveOut.Rooms.Count; i++){
                Textlayout.Children.Add(AddRoom(App.lo.MoveOut.Rooms[i]));
             }
-=======
-            // StackLayout grid = AddHeader();
-            // this.Content = grid;
-            //for (int i = 0; i < App.lo.MoveOut.Rooms.Count; i++){
-            //   Textlayout.Children.Add(AddRoom(App.lo.MoveOut.Rooms[i]));
-            //}
->>>>>>> parent of 9b106ad... none
 
             Textlayout.Children.Add(details);
 
